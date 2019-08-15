99b13d30d0e84002b7b430a5e3ead3360fd33a67
Sarah McLaughlin <sarahbmcl@gmail.com>
2019-08-15 16:27:06 -0500

    added js psuedocode

---
+++ game.js
@@ -0,0 +1,14 @@
+//show random number between 19-120
+
+//4 buttons
+    //click crystal to add specific points to score 1-12
+        //hidden until clicked
+        //click and add to score counter
+
+//win if score matches random score
+    //add 1 win
+//lose if score > random score
+    //add 1 loss
+//restart with win/lose
+    //start with new random number
+    //crystals have new values
